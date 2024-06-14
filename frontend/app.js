57fb95055f79b707c54bd71ea3abd8477eeb1cd6
BlepAn <whyismynametakenagain2@gmail.com>
2024-06-14 17:48:38 +0300

    :DDDD

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
